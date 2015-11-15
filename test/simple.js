36e3cbe676b507a8b85d2e46d5207f0ea7937e53
Markus Felten <Markus.Felten@gmx.de>
2015-11-15 14:25:14 +0100

    chore(test): prepareing for tests

---
+++ test/simple.js
@@ -0,0 +1,13 @@
+/* global describe, it, xit */
+/* jslint node: true, esnext: true */
+
+"use strict";
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should();
+
+describe('t1', function () {
+  assert.equal("a", "b");
+});
