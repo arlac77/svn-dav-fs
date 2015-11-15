9ecfc696d7ea1abe3892e3ab269335103483e2c7
Markus Felten <Markus.Felten@gmx.de>
2015-11-15 13:38:21 +0100

    chore(babel): setup babel6

---
+++ src/svn.js
@@ -0,0 +1,12 @@
+import fetch from 'fetch';
+
+const NS_S					  = 'svn:';
+const NS_D					  = 'DAV:';
+const NS_SVNDAV                 = "http://subversion.tigris.org/xmlns/dav/";
+const NS_SVNDAV_DEPTH           = "http://subversion.tigris.org/xmlns/dav/svn/depth";
+const NS_SVNDAV_MERGINFO        = "http://subversion.tigris.org/xmlns/dav/svn/mergeinfo";
+const NS_SVNDAV_LOG_REVPROPS    = "http://subversion.tigris.org/xmlns/dav/svn/log-revprops";
+const NS_SVNDAV_ATOMIC_REVPROPS = "http://subversion.tigris.org/xmlns/dav/svn/atomic-revprops";
+const NS_SVNDAV_PARTIAL_REPLAY  = "http://subversion.tigris.org/xmlns/dav/svn/partial-replay";
+
+SVN = {};
