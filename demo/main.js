0c07907b08d3e73cf161cf61c69dc1dd2ece8663
Markus Felten <Markus.Felten@gmx.de>
2015-12-23 15:31:03 +0100

    wip

--- demo/main.js
+++ demo/main.js
@@ -3,14 +3,20 @@ import {
 }
 from './helpers';
 import {
-  init
+  init as svn
 }
 from './svn';
 
-let s = init('https://subversion.assembla.com/svn/delivery_notes', {
+let handle = svn('https://subversion.assembla.com/svn/delivery_notes', {
   credentials: {
     user: "arlac77"
   }
+});
+
+handle.then(svn => svn.report('https://subversion.assembla.com/svn/delivery_notes', 0, 100)).then(data => {
+  console.log(data);
+}).catch(e => {
+  console.log(`error: ${e}`);
 });
 
 let elem = document.getElementById('output');
