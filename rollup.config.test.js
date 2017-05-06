13ce9f407fcd8214103ef4a27913d0da200ac824
Markus Felten <Markus.Felten@gmx.de>
2017-05-06 16:55:26 +0200

    chore: add missing rollup.config.test.js from template chore(scripts): update cover=nyc ava from template chore(scripts): update precover=rollup -c rollup.config.test.js from template chore(scripts): update test=ava from template chore(scripts): update pretest=rollup -c rollup.config.test.js from template chore(scripts): update docs=jsdoc2md --configure doc/jsdoc.json -l off -t doc/README.hbs -f src/*.js >README.md from template chore(devDependencies): remove mocha chore(devDependencies): remove chai chore(devDependencies): add jsdoc-babel=^0.3.0 from template chore(devDependencies): add ava=^0.19.1 from template chore(devDependencies): add rollup-plugin-commonjs=^8.0.2 from template chore(devDependencies): add rollup-plugin-node-resolve=^3.0.0 from template chore(devDependencies): add rollup-plugin-multi-entry=^2.0.1 from template chore(devDependencies): add babel-preset-es2015-rollup=^3.0.0 from template fix: updated set from template fix: updated set from template

---
+++ rollup.config.test.js
@@ -0,0 +1,23 @@
+/* jslint node: true, esnext: true */
+
+import babel from 'rollup-plugin-babel';
+import multiEntry from 'rollup-plugin-multi-entry';
+
+export default {
+  entry: 'tests/**/*_test.js',
+  external: ['ava'],
+  plugins: [
+    babel({
+      babelrc: false,
+      plugins: [
+        'transform-async-generator-functions'
+      ],
+      presets: ['es2015-rollup'],
+      exclude: 'node_modules/**'
+    }),
+    multiEntry()
+  ],
+  format: 'cjs',
+  dest: 'tests/build/bundle.js',
+  sourceMap: true
+};
