3958eb72f857aae5ba145326ec4666bd75e98862
Markus Felten <Markus.Felten@gmx.de>
2018-09-06 18:15:28 +0200

    chore(rollup): enable babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,29 @@
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import pkg from './package.json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import cleanup from "rollup-plugin-cleanup";
+import executable from "rollup-plugin-executable";
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
-  plugins: [resolve(), commonjs()],
+  plugins: [
+    babel({
+      runtimeHelpers: false,
+      externalHelpers: true,
+      babelrc: false,
+      plugins: ["@babel/plugin-proposal-async-generator-functions"],
+      exclude: "node_modules/**"
+    }),
+    resolve(),
+    commonjs()
+  ],
 
   output: {
     file: pkg.main,
-    format: 'cjs',
+    format: "cjs",
     interop: false
   },
 
-  external: ['url', 'url-resolver-fs'],
+  external: ["url", "url-resolver-fs"],
   input: pkg.module
 };
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,20 +1,31 @@
-import istanbul from 'rollup-plugin-istanbul';
-
-import multiEntry from 'rollup-plugin-multi-entry';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import istanbul from "rollup-plugin-istanbul";
+import babel from "rollup-plugin-babel";
+import multiEntry from "rollup-plugin-multi-entry";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
-  input: 'tests/**/*-test.js',
-  external: ['ava', 'url', 'url-resolver-fs'],
+  input: "tests/**/*-test.js",
+  external: ["ava", "url", "url-resolver-fs"],
 
-  plugins: [multiEntry(), resolve(), commonjs(), istanbul({
-    exclude: ['tests/**/*-test.js']
-  })],
+  plugins: [
+    multiEntry(),
+    resolve(),
+    commonjs(),
+    babel({
+      runtimeHelpers: false,
+      externalHelpers: true,
+      babelrc: false,
+      plugins: ["@babel/plugin-proposal-async-generator-functions"]
+    }),
+    istanbul({
+      exclude: ["tests/**/*-test.js", "node_modules/**/*"]
+    })
+  ],
 
   output: {
-    file: 'build/bundle-test.js',
-    format: 'cjs',
+    file: "build/bundle-test.js",
+    format: "cjs",
     sourcemap: true,
     interop: false
   }
